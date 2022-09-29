fc23598af98bea53af22c6933f97aa66933b93d6
Alexey Melezhik <melezhik@gmail.com>
2022-09-29 10:24:22 -0500

    right port

--- .tom/tasks/run-docker/task.bash
+++ .tom/tasks/run-docker/task.bash
@@ -4,7 +4,7 @@ status=$(docker inspect --format '{{json .State.Running}}' $name)
 
 if [[ -n $string ]] || [[ $status != "true" ]] ; then
   set -x
-  docker run  --rm -td -p 127.0.0.1:90:8080 \
+  docker run  --rm -td -p 127.0.0.1:8080:80 \
   --add-host=host.docker.internal:host-gateway \
   --name $name alpine
 fi
